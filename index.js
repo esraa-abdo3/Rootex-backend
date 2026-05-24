c639246b1fcd942ca921fe2c8a7618d2f65775cc
esraa-abdo3 <esraaabdalnasserzz@gmail.com>
2026-05-24 21:57:29 +0300

    handle cors with vercel

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cookieParser = require("cookie-parser")
 // midleware
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:3000", 
+  origin: ["http://localhost:3000"," 'https://rotex-front.vercel.app'"], 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
